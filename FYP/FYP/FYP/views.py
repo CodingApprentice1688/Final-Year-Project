202d360765783a8727d33cda95894f1a33e63ecd
leongwenling <leong_wenling@yahoo.com>
2022-06-19 18:53:35 +0800

    wenling

--- FYP/FYP/FYP/views.py
+++ FYP/FYP/FYP/views.py
@@ -129,8 +129,8 @@ def PatientViewAppointment():
     msg = ''
     if 'logged_in' in session:
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        #current=date.today()
-        cursor.execute('SELECT * FROM appointments WHERE username = % s AND nric = % s AND date < "2022-06-18"',  (session['username'], session['nric'], ))
+        current=date.now().year
+        cursor.execute('SELECT * FROM appointments WHERE username = % s AND nric = % s AND date < current',  (session['username'], session['nric'], ))
         userA = cursor.fetchall()
         userB = cursor.fetchone()
         
