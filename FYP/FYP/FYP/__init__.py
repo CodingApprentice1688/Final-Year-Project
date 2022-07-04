3baf15a3f00184eed51ed3d6777e0a429913d3ce
OSWALDO <oswaldoromy@gmail.com>
2022-07-04 14:02:05 +0700

    os

--- FYP/FYP/FYP/__init__.py
+++ FYP/FYP/FYP/__init__.py
@@ -21,4 +21,4 @@ import FYP.controller.PatientViewAppointmentController
 import FYP.controller.PatientCancelAppointmentController
 import FYP.controller.LoginController
 import FYP.controller.LogoutController
-
+import FYP.controller.PatientUpdatePersonalDetailController
