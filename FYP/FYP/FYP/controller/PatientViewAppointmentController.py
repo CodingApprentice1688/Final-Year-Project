c2bc9debb4f1c126dfbefaf20573d2e62bc73334
leongwenling <leong_wenling@yahoo.com>
2022-07-05 10:05:07 +0800

    wenling

--- FYP/FYP/FYP/controller/PatientViewAppointmentController.py
+++ FYP/FYP/FYP/controller/PatientViewAppointmentController.py
@@ -8,9 +8,27 @@ from FYP import mysql
 from flask import Flask,render_template, request, redirect, url_for, Response, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors 
+from flask_classful import FlaskView
 
-@app.route('/PatientViewAppointmentController', methods=['GET', 'POST'])
-def viewAppointment():
-    if 'logged_in' in session: 
-        userA, userB = Appointments.viewAppointment()
-        return render_template('PatientViewAppointment.html', userA = userA, userB = userB)
+
+class PatientViewAppointmentController:
+    def __init__(self, ip, port): 
+            self.ip = ip
+            self.port = port
+            self.app = Flask(__name__)
+            self.some_attribute = 'PatientViewAppointmentController'
+
+    @self.app.route('/PatientViewAppointmentController', methods=['GET', 'POST'])
+    def viewAppointment(self):
+        if 'logged_in' in session: 
+            userA, userB = Appointments.viewAppointment()
+            return render_template('PatientViewAppointment.html', userA = userA, userB = userB)
+
+#PatientViewAppointmentController.register(app,route_base = '/')
+
+#working without class
+#@app.route('/PatientViewAppointmentController', methods=['GET', 'POST'])
+#def viewAppointment():
+#    if 'logged_in' in session: 
+#        userA, userB = Appointments.viewAppointment()
+#        return render_template('PatientViewAppointment.html', userA = userA, userB = userB)
