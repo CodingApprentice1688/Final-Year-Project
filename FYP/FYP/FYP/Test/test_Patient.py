d6395334702a90119a7966a525e8e5c96566f766
leongwenling <leong_wenling@yahoo.com>
2022-07-05 11:25:00 +0800

    wenling

---
+++ FYP/FYP/FYP/Test/test_Patient.py
@@ -0,0 +1,19 @@
+import unittest
+from FYP import app 
+from FYP.entity.User import *
+from FYP.entity.Appointments import *
+from datetime import datetime, date
+from flask import render_template
+from FYP import mysql
+
+from flask import Flask,render_template, request, redirect, url_for, Response, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors 
+
+
+class testPatient(unittest.TestCase):
+    def testLogin():
+        assert username == 'wenling'
+        assert hashed_password != 'password'
+        assert role == 'patient'
+
