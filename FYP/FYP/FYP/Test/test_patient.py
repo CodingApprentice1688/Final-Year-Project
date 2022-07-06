c71fecc8fed453c3f4da1ff532d8376b440ddb1a
leongwenling <leong_wenling@yahoo.com>
2022-07-06 21:08:05 +0800

    wenling

--- FYP/FYP/FYP/Test/test_patient.py
+++ FYP/FYP/FYP/Test/test_patient.py
@@ -1,32 +1,22 @@
 
 import unittest
 from unittest import TestCase
-from entity import User
-#from unittest.mock import patch, MagicMock
-#import mysql.connector
-#from mysql.connector import errorcode
-#from mock import patch
-#from entity.User import User
 from flask import Flask,render_template, request, redirect, url_for, Response, session
-#from flask import Flask, session
 from flask_mysqldb import MySQL
-#from FYP import mysql
 app = Flask(__name__)
 class test_patient(unittest.TestCase):
     def setUp(self):
-       self.app = app
-       self.app.secret_key = 'facial_recognition'
-       self.app.config['MYSQL_HOST'] = 'localhost'
-       self.app.config['MYSQL_USER'] = 'root'
-       self.app.config['MYSQL_PASSWORD'] = ''
-       self.app.config['MYSQL_DB'] = 'healthcare_db' #change into your own database
-
-       #self.app.secret_key = 'facial_recognition'
-       #app.config['SECRET_KEY'] = 'facial_recognition'
+       self.app = Flask(__name__)
+       app.config['SECRET_KEY'] = 'facial_recognition'
        self.client = self.app.test_client()
        self.app = app.test_client()
-       mysql = MySQL(app)
-    
+
+    def test_login(self):
+       with app.test_client() as client:
+           client.post('/LoginController', data=dict(username='s', password='s'))
+           with client.session_transaction() as session:
+               self.assertEquals(session['username'], username)
+
     def test_pass_correct(self):
         tester = app.test_client(self)
         response = tester.post('/LoginController', data=dict(username = 'wenling', password='password'))
@@ -70,3 +60,4 @@ class test_patient(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
