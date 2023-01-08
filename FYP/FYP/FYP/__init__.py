5b61e9d3222f4474d98e53a5a77ec5d77263f9aa
OSWALDO <oswaldoromy@gmail.com>
2023-01-09 05:48:11 +0800

    removed unused lines

--- FYP/FYP/FYP/__init__.py
+++ FYP/FYP/FYP/__init__.py
@@ -19,16 +19,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
 mysql = MySQL(app)
 
 #add firebase into as a new appplication with this config and change into strings dicts
-firebaseConfig = {
-  'apiKey': "AIzaSyDf3ELC9jK3wtA03qEvyAo0XmvTsg_ywMk",
-  'authDomain': "csit321-fc7e5.firebaseapp.com",
-  'databaseURL': "https://csit321-fc7e5-default-rtdb.asia-southeast1.firebasedatabase.app",
-  'projectId': "csit321-fc7e5",
-  'storageBucket': "csit321-fc7e5.appspot.com",
-  'messagingSenderId': "766096353726",
-  'appId': "1:766096353726:web:3034e9ceb29a36e97a31ef",
-  'measurementId': "G-J5DCWLPNM4",
-  "serviceAccount": "FYP/csit321-fc7e5-firebase-adminsdk-m3r1k-df2514abc0.json"}
+firebaseConfig = {}
 
 #init app
 firebase = pyrebase.initialize_app(firebaseConfig)
